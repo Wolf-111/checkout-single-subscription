555eec85afc3daac4b29f54a97a784897d63c996
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2021-11-09 14:45:41 -0800

    init

--- server/node/server.js
+++ server/node/server.js
@@ -64,7 +64,11 @@ app.post("/create-checkout-session", async (req, res) => {
           price: priceId,
           quantity: 1,
         },
+        
       ],
+      phone_number_collection: {
+        enabled: true,
+      },
       // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
       success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${domainURL}/canceled.html`,
